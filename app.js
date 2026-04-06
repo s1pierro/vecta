b4dbb9fb4c37b380b69ee1c9f54d0fea56ca44f5
s1pierro <s1p.tom@gmail.com>
2026-04-06 16:26:37 +0000

    Refactor: rewrite app.js with modern JavaScript classes (ES6+)

--- app.js
+++ app.js
@@ -1,69 +1,73 @@
-console.log('=== VECTA APP.JS LOADING ===');
-window.onerror = function(msg, url, line, col, err) {
-  console.log('ERROR:', msg, 'at line', line);
-};
-var LayoutManager = (function() {
-  function LayoutManager(corePanel, drawArea) {
-    this.corePanel = corePanel;
-    this.drawArea = drawArea;
-    this.appGround = document.getElementById('app-ground');
-    this._init();
+const APP_NAME = 'Vecta';
+
+class LayoutManager {
+  #corePanel;
+  #drawArea;
+  #appGround;
+
+  constructor(corePanel, drawArea) {
+    this.#corePanel = corePanel;
+    this.#drawArea = drawArea;
+    this.#appGround = document.getElementById('app-ground');
+    this.#init();
   }
 
-  LayoutManager.prototype._init = function() {
-    var self = this;
-    window.addEventListener('resize', function() { self._handleResize(); });
-    this._handleResize();
-  };
+  #init() {
+    window.addEventListener('resize', () => this.#handleResize());
+    this.#handleResize();
+  }
 
-  LayoutManager.prototype._handleResize = function() {
-    var isPortrait = window.matchMedia('(orientation: portrait)').matches;
+  #handleResize() {
+    const isPortrait = window.matchMedia('(orientation: portrait)').matches;
     this.applyLayout(isPortrait ? 'portrait' : 'landscape');
-  };
-
-  LayoutManager.prototype.applyLayout = function(orientation) {
-    this.appGround.classList.remove('landscape', 'portrait');
-    this.appGround.classList.add(orientation);
-  };
-
-  return LayoutManager;
-})();
-
-var Application = (function() {
-  function Application(options) {
-    options = options || {};
-    this.container = options.container || document.body;
-    this.statesMachine = new StateMachine();
-    this.corePanel = new CorePanel(this.statesMachine);
-    this._drawArea = new DrawArea(this.statesMachine);
-    this.touchOverlay = null;
-    this.layoutManager = null;
-    
-    this.buildDom(this.container);
-    this._init();
   }
 
-  Application.prototype.buildDom = function(container) {
-    var appGround = document.getElementById('app-ground');
+  applyLayout(orientation) {
+    this.#appGround.classList.remove('landscape', 'portrait');
+    this.#appGround.classList.add(orientation);
+  }
+}
+
+class Application {
+  #statesMachine;
+  #corePanel;
+  #drawArea;
+  #touchOverlay;
+  #layoutManager;
+
+  constructor(options = {}) {
+    this.#statesMachine = new StateMachine();
+    this.#corePanel = new CorePanel(this.#statesMachine);
+    this.#drawArea = new DrawArea(this.#statesMachine);
+
+    this.buildDom(options.container || document.body);
+    this.#init();
+  }
+
+  get drawArea() {
+    return this.#drawArea;
+  }
+
+  buildDom(container) {
+    let appGround = document.getElementById('app-ground');
     if (!appGround) {
       appGround = document.createElement('div');
       appGround.id = 'app-ground';
       appGround.className = 'landscape';
       container.appendChild(appGround);
     }
-    this.domElement = appGround;
 
-    var panelContainer = document.createElement('div');
+    const panelContainer = document.createElement('div');
     panelContainer.id = 'corePanelContainer';
-    this.corePanel.buildDom(panelContainer);
+    this.#corePanel.buildDom(panelContainer);
     appGround.appendChild(panelContainer);
 
-    var drawContainer = document.createElement('div');
+    const drawContainer = document.createElement('div');
     drawContainer.id = 'drawAreaContainer';
-    this._drawArea.buildDom(drawContainer);
+    this.#drawArea.buildDom(drawContainer);
     appGround.appendChild(drawContainer);
 
-    var statusBar = document.createElement('div');
+    const statusBar = document.createElement('div');
     statusBar.id = 'statusBar';
     statusBar.innerHTML = 
       '<span><span class="label">Mode:</span><span class="value" id="statusMode">-</span></span>' +
@@ -72,14 +76,10 @@ var Application = (function() {
       '<span><span class="label">Taille:</span><span class="value" id="statusSize">-</span></span>' +
       '<span><span class="label">Sélection:</span><span class="value" id="statusSelected">-</span></span>';
     container.appendChild(statusBar);
-  };
+  }
 
-  Application.prototype._init = function() {
-    var self = this;
-    var drawAreaContainer = this._drawArea.container;
-    console.log('Creating TouchOverlay on:', drawAreaContainer);
-    
-    this.touchOverlay = new TouchOverlay(this._drawArea.touchOverlayElement, {
+  #init() {
+    this.#touchOverlay = new TouchOverlay(this.#drawArea.touchOverlayElement, {
       dist: 0,
       tappingToPressingFrontier: 600,
       pressingToLongPressingFrontier: 1950,
@@ -89,52 +89,41 @@ var Application = (function() {
       pulseEnabled: true
     });
 
-    console.log('TouchOverlay created:', !!this.touchOverlay);
-    console.log('TouchOverlay.engine:', this.touchOverlay.engine);
+    this.#drawArea.bindDrawEvents(this.#touchOverlay);
+    this.#layoutManager = new LayoutManager(this.#corePanel, this.#drawArea);
 
-    this._drawArea.bindDrawEvents(this.touchOverlay);
-    this.layoutManager = new LayoutManager(this.corePanel, this._drawArea);
+    this.#statesMachine.on('colorChange', () => this.#drawArea._redraw());
+    this.#statesMachine.on('sizeChange', () => this.#drawArea._redraw());
+    this.#statesMachine.on('clearCanvas', () => this.#drawArea._redraw());
 
-    this.statesMachine.on('colorChange', function() { self._drawArea._redraw(); });
-    this.statesMachine.on('sizeChange', function() { self._drawArea._redraw(); });
-    this.statesMachine.on('clearCanvas', function() { self._drawArea._redraw(); });
+    this.#updateStatusBar();
 
-    this._updateStatusBar();
+    this.#statesMachine.on('modeChange', () => this.#updateStatusBar());
+    this.#statesMachine.on('toolChange', () => this.#updateStatusBar());
+    this.#statesMachine.on('colorChange', () => this.#updateStatusBar());
+    this.#statesMachine.on('sizeChange', () => this.#updateStatusBar());
+    this.#statesMachine.on('selectedPathChange', () => this.#updateStatusBar());
+  }
 
-    this.statesMachine.on('modeChange', function() { self._updateStatusBar(); });
-    this.statesMachine.on('toolChange', function() { self._updateStatusBar(); });
-    this.statesMachine.on('colorChange', function() { self._updateStatusBar(); });
-    this.statesMachine.on('sizeChange', function() { self._updateStatusBar(); });
-    this.statesMachine.on('selectedPathChange', function() { self._updateStatusBar(); });
+  #updateStatusBar() {
+    const statusMode = document.getElementById('statusMode');
+    const statusTool = document.getElementById('statusTool');
+    const statusColor = document.getElementById('statusColor');
+    const statusSize = document.getElementById('statusSize');
+    const statusSelected = document.getElementById('statusSelected');
 
-    console.log('Vecta initialized');
-  };
-
-  Application.prototype._updateStatusBar = function() {
-    var statusMode = document.getElementById('statusMode');
-    var statusTool = document.getElementById('statusTool');
-    var statusColor = document.getElementById('statusColor');
-    var statusSize = document.getElementById('statusSize');
-    var statusSelected = document.getElementById('statusSelected');
-    
-    if (statusMode) statusMode.textContent = this.statesMachine.mode;
-    if (statusTool) statusTool.textContent = this.statesMachine.currentTool;
-    if (statusColor) statusColor.textContent = this.statesMachine.currentColor;
-    if (statusSize) statusSize.textContent = this.statesMachine.currentSize;
+    if (statusMode) statusMode.textContent = this.#statesMachine.mode;
+    if (statusTool) statusTool.textContent = this.#statesMachine.currentTool;
+    if (statusColor) statusColor.textContent = this.#statesMachine.currentColor;
+    if (statusSize) statusSize.textContent = this.#statesMachine.currentSize;
     if (statusSelected) {
-      var selected = this.statesMachine.selectedPath;
-      statusSelected.textContent = selected ? (selected.color + ', ' + selected.size + 'px') : '-';
+      const selected = this.#statesMachine.selectedPath;
+      statusSelected.textContent = selected ? `${selected.color}, ${selected.size}px` : '-';
     }
-  };
+  }
+}
 
-  Object.defineProperty(Application.prototype, 'drawArea', {
-    get: function() { return this._drawArea; }
-  });
-
-  return Application;
-})();
-
-var app = null;
-document.addEventListener('DOMContentLoaded', function() {
+let app = null;
+document.addEventListener('DOMContentLoaded', () => {
   app = new Application();
 });
